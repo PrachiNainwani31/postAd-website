47e34be127000f49196c23d4e0f27bae0227e597
PrachiNainwani31 <prachinainwnai31@gmail.com>
2025-07-26 03:10:00 +0530

    connected vercel

--- frontend/PostAd/src/api/api.js
+++ frontend/PostAd/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiUrl = process.env.VITE_API_URL; 
+const apiUrl = import.meta.env.VITE_API_URL; 
 
 // This is the correct and simplest way
 const API = axios.create({
